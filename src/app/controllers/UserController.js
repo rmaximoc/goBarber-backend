f2d6774dc248732d68030c418d2421402e93abb6
rmaximoc <rodrigomaximocano@gmail.com>
2019-09-10 15:35:30 -0300

    Firts commit

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,25 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'user already exists' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b2761e76cf610ea25025f2695787baa9',
+  expiresIn: '30d',
+};
