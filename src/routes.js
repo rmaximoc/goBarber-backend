5d0d786feb0d2e566c575fa18e99187d779aeba3
rmaximoc <rodrigomaximocano@gmail.com>
2019-10-03 12:02:31 -0300

    Add appointment functionality

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,9 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-import ProviderController from './app/controllers/ProviderController'
+import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,7 +19,9 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.get('/providers', ProviderController);
+routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.sinlge('file'), FileController.store);
 
