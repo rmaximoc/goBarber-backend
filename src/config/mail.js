98766d7b4bfc6ff49d0024c33b190d7a34245963
rmaximoc <rodrigomaximocano@gmail.com>
2019-10-14 11:19:11 -0300

    Add mail sender lib and configuration

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9bd9423bcc6aeb',
+    pass: 'b6e15e3aea224a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
